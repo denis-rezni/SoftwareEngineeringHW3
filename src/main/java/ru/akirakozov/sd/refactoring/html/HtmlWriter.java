a832fe8cfb96dc689bfd481f52c6befeb9604093
denisrezni <denisrezni@yandex-team.ru>
2021-11-01 19:24:24 +0300

    add constants for opening and closing tags

--- src/main/java/ru/akirakozov/sd/refactoring/html/HtmlWriter.java
+++ src/main/java/ru/akirakozov/sd/refactoring/html/HtmlWriter.java
@@ -10,36 +10,39 @@ public class HtmlWriter {
 
     private final HttpServletResponse response;
 
+    private final String OPENING_TAGS = "<html><body>";
+    private final String CLOSING_TAGS = "</body></html>";
+
     public HtmlWriter(HttpServletResponse response) {
         this.response = response;
     }
 
     public void writeGet(List<Item> items) throws IOException {
-        response.getWriter().println("<html><body>");
+        response.getWriter().println(OPENING_TAGS);
         for (Item item : items) {
             response.getWriter().println(item.getName() + "\t" + item.getPrice() + "</br>");
         }
-        response.getWriter().println("</body></html>");
+        response.getWriter().println(CLOSING_TAGS);
     }
 
     public void writeMin(Item minItem) throws IOException {
-        response.getWriter().println("<html><body>");
+        response.getWriter().println(OPENING_TAGS);
         response.getWriter().println("<h1>Product with min price: </h1>");
         response.getWriter().println(minItem.getName() + "\t" + minItem.getPrice() + "</br>");
-        response.getWriter().println("</body></html>");
+        response.getWriter().println(CLOSING_TAGS);
     }
 
     public void writeSum(long sum) throws IOException {
-        response.getWriter().println("<html><body>");
+        response.getWriter().println(OPENING_TAGS);
         response.getWriter().println("Summary price: ");
         response.getWriter().println(sum);
-        response.getWriter().println("</body></html>");
+        response.getWriter().println(CLOSING_TAGS);
     }
 
     public void writeMax(Item maxItem) throws IOException {
-        response.getWriter().println("<html><body>");
+        response.getWriter().println(OPENING_TAGS);
         response.getWriter().println("<h1>Product with max price: </h1>");
         response.getWriter().println(maxItem.getName() + "\t" + maxItem.getPrice() + "</br>");
-        response.getWriter().println("</body></html>");
+        response.getWriter().println(CLOSING_TAGS);
     }
 }
